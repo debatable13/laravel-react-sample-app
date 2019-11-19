86330f1398c98b84d99d2bd77868676b000c2631
Hajime Ikeda <hajime.ikeda@persol.co.jp>
2019-11-19 06:32:59 +0000

    Fix it

--- resources/js/screens/example/AddResourceForm.js
+++ resources/js/screens/example/AddResourceForm.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import { Modal, Form, Input, Radio } from 'antd';
+import 'antd/lib/style/index.css';
+import 'antd/lib/modal/style/index.css';
+import 'antd/lib/button/style/index.css'
+import 'antd/lib/form/style/index.css'
+import 'antd/lib/input/style/index.css'
+
 const FormItem = Form.Item;
 
-
 const AddResourceForm = Form.create()(
-    (props) => {
-        const { visible, onCancel, onCreate, form } = props;
-        const { getFieldDecorator } = form;
-        return (
-            <Modal
-                visible={visible}
-                title="New Resource"
-                okText="Create"
-                onCancel={onCancel}
-                onOk={onCreate}
-            >
-                <Form layout="vertical" >
-                    <FormItem label="Name">
-                        {getFieldDecorator('name', {
-                            rules: [{ required: true, message: 'Please input the name of the resource!' }],
-                        })(
-                            <Input />
-                        )}
-                    </FormItem>
-                </Form>
-            </Modal>
-        );
-    }
+  (props) => {
+    const { visible, onCancel, onCreate, form } = props;
+    const { getFieldDecorator } = form;
+    return (
+      <Modal
+        visible={visible}
+        title="新規ユーザーを追加"
+        okText="追加"
+        onCancel={onCancel}
+        onOk={onCreate}
+      >
+        <Form layout="vertical">
+          <FormItem label="Name">
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '名前は必須入力項目です！' }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+        </Form>
+      </Modal>
+    );
+  }
 );
 
 export default AddResourceForm
--- resources/js/screens/shift.js
+++ resources/js/screens/shift.js
@@ -2,200 +2,261 @@ import React, {Component} from 'react'
 import Scheduler, {SchedulerData, ViewTypes, DATE_FORMAT, DemoData} from 'react-big-scheduler'
 import 'react-big-scheduler/lib/css/style.css'
 import moment from 'moment'
-import Nav from './example/Nav'
-import ViewSrcCode from './example/ViewSrcCode'
 import withDragDropContext from './example/withDnDContext'
 import AddResourceForm from './example/AddResourceForm'
 
 class Shift extends Component{
-    constructor(props){
-        super(props);
-        let today = moment().format(DATE_FORMAT);
-        let schedulerData = new SchedulerData(today, ViewTypes.Week);
-        schedulerData.localeMoment.locale('en');
-        schedulerData.setResources(DemoData.resources);
-        schedulerData.setEvents(DemoData.events);
-        this.state = {
-            viewModel: schedulerData,
-            visible: false
-        }
+  constructor(props){
+    super(props);
+    console.log(DemoData);
+
+    moment.locale('ja');
+    let schedulerData = new SchedulerData(new moment().format(DATE_FORMAT), ViewTypes.Day, false, false, {
+      resourceName: 'ユーザー名',
+      dayMaxEvents: 1,
+      monthMaxEvents: 1,
+      addMorePopoverHeaderFormat: 'YYYY年M月D日 dddd',
+      eventItemPopoverDateFormat: 'M月D日',
+      nonAgendaDayCellHeaderFormat: 'HH',
+      nonAgendaOtherCellHeaderFormat: 'D（ddd）',
+      views: [
+        {viewName: '日', viewType: ViewTypes.Day, showAgenda: false, isEventPerspective: false},
+        {viewName: '月', viewType: ViewTypes.Month, showAgenda: false, isEventPerspective: false},
+      ]
+    }, {
+      getDateLabelFunc: this.getDateLabel,
+      isNonWorkingTimeFunc: this.isNonWorkingTime
+    }, moment)
+    schedulerData.localeMoment.locale('ja');
+    schedulerData.setResources(DemoData.resources);
+    schedulerData.setEvents(DemoData.events);
+    schedulerData.setMinuteStep(15);
+    
+    this.state = {
+      viewModel: schedulerData
     }
-    showModal = () => {
-        this.setState({ visible: true });
+  }
+  showModal = () => {
+    this.setState({ visible: true });
+  }
+  handleCancel = () => {
+    this.setState({ visible: false });
+  }
+  handleCreate = () => {
+    const form = this.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      this.addResource(values.name)
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+    
+  }
+  saveFormRef = (form) => {
+    this.form = form;
+  }
+
+  render(){
+    const {viewModel} = this.state;
+
+    let leftCustomHeader = (
+      <div>
+        <span style={{ fontWeight: 'bold' }}><a onClick={this.showModal}>ユーザーを追加</a></span>
+        <AddResourceForm
+          ref={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+          addResource={this.addResource}
+        />
+      </div>
+    );
+
+    return (
+      <div>
+        <h3 style={{textAlign: 'center'}}>Calender Sample</h3>
+        <Scheduler 
+          schedulerData={viewModel}
+          prevClick={this.prevClick}
+          nextClick={this.nextClick}
+          onSelectDate={this.onSelectDate}
+          onViewChange={this.onViewChange}
+          eventItemClick={this.eventClicked}
+          viewEventClick={this.ops1}
+          viewEventText="Ops 1"
+          viewEvent2Text="Ops 2"
+          viewEvent2Click={this.ops2}
+          updateEventStart={this.updateEventStart}
+          updateEventEnd={this.updateEventEnd}
+          moveEvent={this.moveEvent}
+          newEvent={this.newEvent}
+          leftCustomHeader={leftCustomHeader}
+          eventItemTemplateResolver={this.eventItemTemplateResolver}
+          toggleExpandFunc={this.toggleExpandFunc}
+        />
+      </div>
+    )
+  }
+
+  prevClick = (schedulerData)=> {
+    schedulerData.prev();
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  nextClick = (schedulerData)=> {
+    schedulerData.next();
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  onViewChange = (schedulerData, view) => {
+    schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  onSelectDate = (schedulerData, date) => {
+    schedulerData.setDate(date);
+    schedulerData.setEvents(DemoData.events);
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  eventClicked = (schedulerData, event) => {
+    alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}`);
+  };
+
+  ops1 = (schedulerData, event) => {
+    alert(`You just executed ops1 to event: {id: ${event.id}, title: ${event.title}}`);
+  };
+
+  ops2 = (schedulerData, event) => {
+    alert(`You just executed ops2 to event: {id: ${event.id}, title: ${event.title}}`);
+  };
+
+  newEvent = (schedulerData, slotId, slotName, start, end, type, item) => {
+    if(confirm(`Do you want to create a new event? {slotId: ${slotId}, slotName: ${slotName}, start: ${start}, end: ${end}, type: ${type}, item: ${item}}`)){
+
+      let newFreshId = 0;
+      schedulerData.events.forEach((item) => {
+        if(item.id >= newFreshId)
+          newFreshId = item.id + 1;
+      });
+
+      let newEvent = {
+        id: newFreshId,
+        title: 'New event you just created',
+        start: start,
+        end: end,
+        resourceId: slotId,
+        bgColor: 'purple'
+      }
+      schedulerData.addEvent(newEvent);
+      this.setState({
+        viewModel: schedulerData
+      })
     }
-    handleCancel = () => {
-        this.setState({ visible: false });
-    }
-    handleCreate = () => {
-        const form = this.form;
-        form.validateFields((err, values) => {
-            if (err) {
-                return;
-            }
-            this.addResource(values.name)
-            form.resetFields();
-            this.setState({ visible: false });
-        });
-        
-    }
-    saveFormRef = (form) => {
-        this.form = form;
-    }
+  }
 
-    render(){
-        const {viewModel} = this.state;
+  updateEventStart = (schedulerData, event, newStart) => {
+    if(confirm(`Do you want to adjust the start of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newStart: ${newStart}}`)) {
+      schedulerData.updateEventStart(event, newStart);
+    }
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
 
-        let leftCustomHeader = (
-            <div>
-                <span style={{ fontWeight: 'bold' }}><a onClick={this.showModal}>Add a resource</a></span>
-                <AddResourceForm
-                    ref={this.saveFormRef}
-                    visible={this.state.visible}
-                    onCancel={this.handleCancel}
-                    onCreate={this.handleCreate}
-                    addResource={this.addResource}
-                />
-            </div>
-        );
+  updateEventEnd = (schedulerData, event, newEnd) => {
+    if(confirm(`Do you want to adjust the end of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newEnd: ${newEnd}}`)) {
+      schedulerData.updateEventEnd(event, newEnd);
+    }
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
 
-        return (
-            <div>
-                <Nav />
-                <div>
-                    <h3 style={{textAlign: 'center'}}>Add resource<ViewSrcCode srcCodeUrl="https://github.com/StephenChou1017/react-big-scheduler/blob/master/example/AddResource.js" /></h3>
-                    <Scheduler schedulerData={viewModel}
-                               prevClick={this.prevClick}
-                               nextClick={this.nextClick}
-                               onSelectDate={this.onSelectDate}
-                               onViewChange={this.onViewChange}
-                               eventItemClick={this.eventClicked}
-                               viewEventClick={this.ops1}
-                               viewEventText="Ops 1"
-                               viewEvent2Text="Ops 2"
-                               viewEvent2Click={this.ops2}
-                               updateEventStart={this.updateEventStart}
-                               updateEventEnd={this.updateEventEnd}
-                               moveEvent={this.moveEvent}
-                               newEvent={this.newEvent}
-                               leftCustomHeader={leftCustomHeader}
-                               toggleExpandFunc={this.toggleExpandFunc}
-                    />
-                </div>
-            </div>
-        )
+  moveEvent = (schedulerData, event, slotId, slotName, start, end) => {
+    if(confirm(`Do you want to move the event? {eventId: ${event.id}, eventTitle: ${event.title}, newSlotId: ${slotId}, newSlotName: ${slotName}, newStart: ${start}, newEnd: ${end}`)) {
+      schedulerData.moveEvent(event, slotId, slotName, start, end);
+      this.setState({
+        viewModel: schedulerData
+      })
+    }
+  }
+
+  getDateLabel = (schedulerData, viewType, startDate, endDate) => {
+    let start = schedulerData.localeMoment(startDate);
+    let end = schedulerData.localeMoment(endDate);
+    let dateLabel = start.format('YYYY年M月D日（ddd）');
+
+    if (viewType === ViewTypes.Month) {
+      dateLabel = start.format('YYYY年M月');
     }
 
-    prevClick = (schedulerData)=> {
-        schedulerData.prev();
-        schedulerData.setEvents(DemoData.events);
-        this.setState({
-            viewModel: schedulerData
-        })
+    return dateLabel;
+}
+
+isNonWorkingTime = (schedulerData, time) => {
+    const { localeMoment } = schedulerData;
+    if(schedulerData.viewType === ViewTypes.Day){
+        let hour = localeMoment(time).hour();
+        if(hour < 10 || hour > 19)
+            return true;
+    }
+    else {
+        let dayOfWeek = localeMoment(time).weekday();
+        if (dayOfWeek === 5 || dayOfWeek === 6)
+            return true;
     }
 
-    nextClick = (schedulerData)=> {
-        schedulerData.next();
-        schedulerData.setEvents(DemoData.events);
-        this.setState({
-            viewModel: schedulerData
-        })
+    return false;
+}
+
+  addResource = (resourceName) => {
+    let schedulerData = this.state.viewModel;
+    let newFreshId = schedulerData.resources.length + 1;
+    let newFreshName = resourceName;
+    schedulerData.addResource({id: newFreshId, name: newFreshName});
+    this.setState({
+      viewModel: schedulerData
+    })
+  }
+
+  eventItemTemplateResolver = (schedulerData, event, bgColor, isStart, isEnd, mustAddCssClass, mustBeHeight, agendaMaxEventWidth) => {
+    let borderWidth = isStart ? '4' : '0';
+    let borderColor =  'rgba(0,139,236,1)', backgroundColor = '#80C5F6';
+    let titleText = schedulerData.behaviors.getEventTextFunc(schedulerData, event);
+    if(!!event.type) {
+      borderColor = event.type == 1 ? 'rgba(0,139,236,1)' : (event.type == 3 ? 'rgba(245,60,43,1)' : '#999');
+      backgroundColor = event.type == 1 ? '#80C5F6' : (event.type == 3 ? '#FA9E95' : '#D9D9D9');
+    }
+    let divStyle = {borderLeft: borderWidth + 'px solid ' + borderColor, backgroundColor: backgroundColor, height: mustBeHeight };
+    if(!!agendaMaxEventWidth) {
+      divStyle = {...divStyle, maxWidth: agendaMaxEventWidth};
     }
 
-    onViewChange = (schedulerData, view) => {
-        schedulerData.setViewType(view.viewType, view.showAgenda, view.isEventPerspective);
-        schedulerData.setEvents(DemoData.events);
-        this.setState({
-            viewModel: schedulerData
-        })
-    }
+    return <div key={event.id} className={mustAddCssClass} style={divStyle}>
+      <span style={{marginLeft: '4px', lineHeight: `${mustBeHeight}px` }}>{titleText}</span>
+    </div>;
+}
 
-    onSelectDate = (schedulerData, date) => {
-        schedulerData.setDate(date);
-        schedulerData.setEvents(DemoData.events);
-        this.setState({
-            viewModel: schedulerData
-        })
-    }
-
-    eventClicked = (schedulerData, event) => {
-        alert(`You just clicked an event: {id: ${event.id}, title: ${event.title}}`);
-    };
-
-    ops1 = (schedulerData, event) => {
-        alert(`You just executed ops1 to event: {id: ${event.id}, title: ${event.title}}`);
-    };
-
-    ops2 = (schedulerData, event) => {
-        alert(`You just executed ops2 to event: {id: ${event.id}, title: ${event.title}}`);
-    };
-
-    newEvent = (schedulerData, slotId, slotName, start, end, type, item) => {
-        if(confirm(`Do you want to create a new event? {slotId: ${slotId}, slotName: ${slotName}, start: ${start}, end: ${end}, type: ${type}, item: ${item}}`)){
-
-            let newFreshId = 0;
-            schedulerData.events.forEach((item) => {
-                if(item.id >= newFreshId)
-                    newFreshId = item.id + 1;
-            });
-
-            let newEvent = {
-                id: newFreshId,
-                title: 'New event you just created',
-                start: start,
-                end: end,
-                resourceId: slotId,
-                bgColor: 'purple'
-            }
-            schedulerData.addEvent(newEvent);
-            this.setState({
-                viewModel: schedulerData
-            })
-        }
-    }
-
-    updateEventStart = (schedulerData, event, newStart) => {
-        if(confirm(`Do you want to adjust the start of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newStart: ${newStart}}`)) {
-            schedulerData.updateEventStart(event, newStart);
-        }
-        this.setState({
-            viewModel: schedulerData
-        })
-    }
-
-    updateEventEnd = (schedulerData, event, newEnd) => {
-        if(confirm(`Do you want to adjust the end of the event? {eventId: ${event.id}, eventTitle: ${event.title}, newEnd: ${newEnd}}`)) {
-            schedulerData.updateEventEnd(event, newEnd);
-        }
-        this.setState({
-            viewModel: schedulerData
-        })
-    }
-
-    moveEvent = (schedulerData, event, slotId, slotName, start, end) => {
-        if(confirm(`Do you want to move the event? {eventId: ${event.id}, eventTitle: ${event.title}, newSlotId: ${slotId}, newSlotName: ${slotName}, newStart: ${start}, newEnd: ${end}`)) {
-            schedulerData.moveEvent(event, slotId, slotName, start, end);
-            this.setState({
-                viewModel: schedulerData
-            })
-        }
-    }
-
-    addResource = (resourceName) => {
-        let schedulerData = this.state.viewModel;
-        let newFreshId = schedulerData.resources.length + 1;
-        let newFreshName = resourceName;
-        schedulerData.addResource({id: newFreshId, name: newFreshName});
-        this.setState({
-            viewModel: schedulerData
-        })
-    }
-
-    toggleExpandFunc = (schedulerData, slotId) => {
-        schedulerData.toggleExpandStatus(slotId);
-        this.setState({
-            viewModel: schedulerData
-        });
-    }
+  toggleExpandFunc = (schedulerData, slotId) => {
+    schedulerData.toggleExpandStatus(slotId);
+    this.setState({
+      viewModel: schedulerData
+    });
+  }
 }
 
 export default withDragDropContext(Shift)
